8c5e9a0725c0f151fe94a7527389230eb67da687
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-07-29 00:12:01 +0700

    replace settings.html

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,35 @@
 <script lang="ts">
-  import InputTable from "./InputTable.svelte";
+    import InputTable from "./InputTable.svelte";
 
-  let rows = ["hello", "world"];
-  document.addEventListener("DOMContentLoaded", restoreOptions);
+    let rows = [];
+    document.addEventListener("DOMContentLoaded", restoreOptions);
 
-  function saveOptions() {
-    const keywordsJSON = JSON.stringify(rows);
+    function saveOptions() {
+        const keywordsJSON = JSON.stringify(rows);
 
-    browser.storage.local.set({
-      keywordsJSON: keywordsJSON,
-    });
-  }
-
-  async function restoreOptions() {
-    var res = await browser.storage.local.get("keywordsJSON");
-
-    if (res.keywordsJSON == null) {
-      rows = [];
-      return;
+        browser.storage.local.set({
+            keywordsJSON: keywordsJSON,
+        });
     }
-    const keywordsJSON = res.keywordsJSON.toString();
 
-    rows = JSON.parse(keywordsJSON);
-  }
+    async function restoreOptions() {
+        const res = await browser.storage.local.get("keywordsJSON");
+
+        if (res.keywordsJSON == null) {
+            return;
+        }
+        const keywordsJSON = res.keywordsJSON.toString();
+
+        rows = JSON.parse(keywordsJSON);
+    }
 </script>
 
 <main>
-  <InputTable {rows} />
-  <button on:click="{()=>saveOptions()}">Save</button>
+    <header>
+        <h1>custom rarbg extension - Configurations</h1>
+    </header>
+    <InputTable {rows}/>
+    <footer>
+        <button on:click="{()=>saveOptions()}">Save</button>
+    </footer>
 </main>
